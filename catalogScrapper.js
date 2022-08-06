a1a5060a158592c1a37453f82f551d459eeff8fc
andresrokp <afrokap@gmail.com>
2022-08-06 08:50:33 -0500

    goto several url - for loop with url string generation

--- catalogScrapper.js
+++ catalogScrapper.js
@@ -1,20 +1,19 @@
-const pptr = require('puppeteer')
+const pptr = require('puppeteer');
 console.log("-\n-----\n---------\n-----\n-");
 
 ( async ()=>{
-    const browser = await pptr.launch()
-    const page = await browser.newPage()
-
-    let url = "https://www.gprindustrial.com/es/16-correas-en-v-tipo-a?order=product.price.asc&page=1";
+    const browser = await pptr.launch();
+    var page = await browser.newPage();
+    // let pgN = 9
+    for(let pgN = 1; pgN < 9; pgN++){
+        let url = `https://www.gprindustrial.com/es/16-correas-en-v-tipo-a?order=product.price.asc&page=${pgN}`;
+        await page.goto(url, {waitUntil: "domcontentloaded"});
+        let data = await page.evaluate(()=>{
+            return [...document.getElementsByClassName("product-title")].map( tit => {return tit.innerText});
+        });
+        console.log(data);
+    };
     
-    await page.goto(url, {waitUntil: "domcontentloaded"})
-
-    let data = await page.evaluate(()=>{
-        return [...document.getElementsByClassName("product-title")].map( tit => {return tit.innerText})
-    })
-    
-    console.log(data)
-
-    browser.close()
+    browser.close();
 
 })();
