d737109425e805e808ce37a3498343a09f13c36c
andresrokp <afrokap@gmail.com>
2022-09-24 13:56:29 -0500

    function help text

--- checkerForAppScript.js
+++ checkerForAppScript.js
@@ -1,10 +1,10 @@
 /**
- * Calculates the sale price of a value at a given discount.
- * The sale price is formatted as US dollars.
+ * Contabiliza la aparición de los términos de busqueda dentro
+ * de cada elemento en el rango seleccionado
  *
- * @param {number} input The value to discount.
- * @param {number} discount The discount to apply, such as .5 or 50%.
- * @return The sale price formatted as USD.
+ * @param {range} a range of strings to be searched separated by spaces
+ * @param {string} an string with search terms to look into the array
+ * @return A sorted array woth ocurrence counting of contained tokens
  * @customfunction
  */
 
@@ -20,7 +20,7 @@ function akapChecker(range){
             } 
         }
     }
-    
+
     let keysArray = Object.keys(resultDict)
     let valuesArray = Object.values(resultDict)
     let resultMatrix = keysArray.map( (name, idx) => [name,valuesArray[idx]])
