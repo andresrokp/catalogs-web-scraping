13498e8d4d1ae253a959639e0cc21776cc5a530f
andresrokp <afrokap@gmail.com>
2022-10-03 15:41:25 -0500

    adding classesCounter() to count first words

--- checkerForAppScript.js
+++ checkerForAppScript.js
@@ -1,6 +1,7 @@
 /**
  * Contabiliza la aparición de los términos de busqueda dentro
- * de cada elemento en el rango seleccionado
+ * de cada elemento en el rango seleccionado.
+ * Por: andresrokp
  *
  * @param {range} a range of strings to be searched separated by spaces
  * @param {string} an string with search terms to look into the array
@@ -8,11 +9,11 @@
  * @customfunction
  */
 
-function akapChecker(range){
-    let searchStr = searchKey.toUpperCase()
+ function afrpChecker(range,searchKey){
+    let searchStr = searchKey.toUpperCase().replace(",","")
     let resourcesArray = range.flat()
     let resultDict = {}
-
+  
     for(let rsr of resourcesArray){
         for(let word of searchStr.split(" ")){
             if(rsr.includes(word)){
@@ -20,13 +21,36 @@ function akapChecker(range){
             } 
         }
     }
-
     let keysArray = Object.keys(resultDict)
     let valuesArray = Object.values(resultDict)
-    let resultMatrix = keysArray.map( (name, idx) => [name,valuesArray[idx]])
-
-    return resultMatrix.sort( (pairA, pairB) => pairB[1] - pairA[1])
+    let resultMatrix = keysArray.map( (name, idx) => [valuesArray[idx], name])
+  
+    return resultMatrix.sort( (pairA, pairB) => pairB[0] - pairA[0])
     // return range.filter(n => n[0].includes()) // JSON.stringify(range)
     // return [["a",1],["b",2],["c",3]];   --->>> succesful data structure for get a matrix
     // return range.flat()
-}
+  }
+  
+  /**
+   * Contabiliza las primeras palabras de los items
+   * Por: andresrokp
+   *
+   * @param {range} a range of strings to be searched separated by spaces
+   * @return A sorted array with counting of first words
+   * @customfunction
+   */
+  
+  function classesCounter(range){
+    let resourcesArray = range.flat()
+    let resultDict = {}
+  
+    for(let rsr of resourcesArray){
+        let className = rsr.split(",")[0].split(" ")[0]
+        resultDict[className] = (resultDict[className] || 0) + 1
+    }
+    let keysArray = Object.keys(resultDict)
+    let valuesArray = Object.values(resultDict)
+    let resultMatrix = keysArray.map( (name, idx) => [valuesArray[idx], name])
+  
+    return resultMatrix.sort( (pairA, pairB) => pairB[0] - pairA[0])
+  }
